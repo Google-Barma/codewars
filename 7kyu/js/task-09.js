bca87b94b494322dca7a110c6e64c76bb81b535c
Vadym Yevlanov <vad.evlanov@gmail.com>
2020-08-24 13:13:54 +0300

    add task9 in 7kuy

---
+++ 7kyu/js/task-09.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function friend(friends) {
+  return friends.filter(f => f.length === 4);
+}
+
+console.log(friend(['Ryan', 'Kieran', 'Mark'])); //, ["Ryan", "Mark"]);
+console.log(friend(['Ryan', 'Jimmy', '123', '4', 'Cool Man'])); //, ["Ryan"]);
+console.log(
+  friend(['Jimm', 'Cari', 'aret', 'truehdnviegkwgvke', 'sixtyiscooooool']),
+); //, ["Jimm", "Cari", "aret"]);
+console.log(friend(['Love', 'Your', 'Face', '1'])); //, ["Love", "Your", "Face"]);
